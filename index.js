e3aeb5d74fbd321f2d073785e99c52531de619a2
kevmsutton <kevmsutton@hotmail.co.uk>
2018-10-17 16:48:29 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function
+sayHelloTo(firstName){
+  console.log(`Hello ${firstName}!`)
+}
+sayHelloTo ('Isabel')
+sayHelloTo ('Johhny')
+sayHelloTo ('r2d2')
+sayHelloTo (1)
+
+function say(greeting, firstName) {
+  console.log(`${greeting}, ${firstName}!`)
+}
+say("Julio", "Hello") 
